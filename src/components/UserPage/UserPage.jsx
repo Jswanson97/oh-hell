9e2d7fef0f624753ca45699dd476a4e750406925
Jesse Swanson <jswanson97@gmail.com>
2024-02-08 12:19:52 -0600

    added button on user page to bring you to Create Game page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const history = useHistory()
+  const handleClick = () => {
+    history.push('/createGame')
+  }
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <button onClick={handleClick}>
+        Create Game
+        </button>
       <LogOutButton className="btn" />
     </div>
   );
